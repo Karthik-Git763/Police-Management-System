ff88cad3443b591cfbfa8e7ad3586c722582da9e
Karthik-Git763 <karthikpilakat@gmail.com>
2024-10-18 13:51:10 +0530

    Resolved merge conflicts

--- police/models.py
+++ police/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from ..civilian.models import CustomUser
-from ..policeAdmin.models import AdminModel
+from civilian.models import CustomUser
+from policeAdmin.models import AdminModel
 # Create your models here.
 
 class Station(models.Model):
