681f2b612603e9c0021df3acc16da4f797cc0055
Joel <joeljosephcl10@gmail.com>
2024-10-19 22:19:22 +0530

    updatedview_crime_details and added cancel_current_crime

--- police/urls.py
+++ police/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', views.logout_police, name='policeLogout'),
     path('selectCrime/',views.view_pending_crimes,name="pendingCrimes"),
     path("crimeDetail/<int:pk>",views.view_crime_details,name="crime"),
+    path("cancelCrime/",views.cancel_current_crime,name="cancelCrime"),
 ]
--- police/views.py
+++ police/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect   #changes in this page after commit
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required,user_passes_test
 from .models import Crime,PoliceModel
+from django.db.models import Q
+
 # Create your views here.
 
 def is_police(user):
     return user.user_type=="Police"
+
+def has_current_crime(user):
+    police=PoliceModel.objects.get(user=user)
+    return police.current_crime is not None
 
 def login_police(request):
     error_message = None
@@ -38,7 +44,7 @@ def home_police(request):
 @login_required(login_url="policeLogin")
 @user_passes_test(is_police,login_url="policeLogin")
 def view_pending_crimes(request):
-    crimes=Crime.objects.filter(status="Request Pending")
+    crimes=Crime.objects.filter(Q(status="Request Pending")| Q(status="Investigating"))
     return render(request,"police_template/pendingCrimes.html",{"crimes":crimes})
 
 @login_required(login_url="policeLogin")
@@ -47,10 +53,37 @@ def view_crime_details(request,pk): #later check if the current police has alrea
     crime=Crime.objects.get(pk=pk)
     if request.method=="POST":
         police=PoliceModel.objects.get(user=request.user)
+        if police.current_crime is not None:
+            return render(request,"police_template/confirmCurrentCrimeChange.html",{"crime":crime})#change to render to different giving option to change and go to home
         police.current_crime=crime
         police.save()
         crime.status="Investigating"
         crime.save()
         return redirect("policeHome")
     else:
+        #user=request.user
+        # police=PoliceModel.objects.get(user=user)
+        # if police.current_crime is not None:
+        #     return render(request,)
         return render(request,"police_template/crimeDetails.html",{"crime":crime})
+
+@login_required(login_url="policeLogin")
+@user_passes_test(is_police,login_url="policeLogin")
+@user_passes_test(has_current_crime,login_url="pendingCrimes")
+def cancel_current_crime(request):
+    user=request.user
+    police=PoliceModel.objects.get(user=user)
+    crime=police.current_crime
+    if request.method=="POST":
+        police.current_crime=None
+        #if no one else is doing that crime change to pending else keep it as investivating 
+        crime.status=request.POST.get("status")#add status choice in form
+        #check later if needed
+        # if crime.status=="Similiar Repoted"
+        #      change for all who have selected this a crime
+        police.save()
+        crime.save()
+        return redirect("policeHome")
+    else:
+        return render(request,"police_template/cancelCrime.html",{"crime":crime})
+    
