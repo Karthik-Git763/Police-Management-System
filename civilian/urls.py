c39e560f12ec737cd00fc039832fa83a5ca74eb4
Joel <joeljosephcl10@gmail.com>
2024-11-01 00:00:35 +0530

    allow user to view crime status and details

--- civilian/urls.py
+++ civilian/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_civilian, name='civilianHome'),
-    path('register/', views.register_civilian, name='civilianRegister'),
-    path('login/', views.login_civilian, name='civilianLogin'),
-    path('logout/', views.logout_civilian, name='civilianLogout'),
-    path('addCrime/', views.add_Crime, name='addCrime'),
-    path('addCrime/success/', views.add_Crime_success, name='success')
+    path("", views.home_civilian, name="civilianHome"),
+    path("register/", views.register_civilian, name="civilianRegister"),
+    path("login/", views.login_civilian, name="civilianLogin"),
+    path("logout/", views.logout_civilian, name="civilianLogout"),
+    path("addCrime/", views.add_Crime, name="addCrime"),
+    path("addCrime/success/", views.add_Crime_success, name="success"),
+    path("submittedCrimes", views.crime_submitted, name="submittedCrimes"),
+    path(
+        "submittedCrimes/<int:pk>",
+        views.submitted_crime_details,
+        name="submittedDetails",
+    ),
 ]
+
